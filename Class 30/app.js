d5084d34989d55062ea865ad24e961150956c5c5
sohaibmumtaz78 <sohaibmumtaz782gmail.com>
2025-03-01 17:39:12 +0500

    practice mouseover/mouseout

---
+++ Class 30/app.js
@@ -0,0 +1,51 @@
+// console.log("Hello World")
+
+// function mouseIn(khuchb){
+//     var Image = document.getElementById("img")
+//     // console.log("img" ,  Image)
+//     khuchb.src = "./images/car (2).jpg"
+// }
+
+// function mouseout(khuchb){
+//     // var Image = document.getElementById(img)
+// }
+
+// function mouseIn( khuchb){
+//     // console.log(event , ele)\
+//     // console.log("img" , khuchb)
+//     khuchb.src = "./images/car (1).jpg";
+// }
+
+// function mouseout(out){
+//     // console.log("img out" , out);
+//     out.src = "./images/car (2).jpg";
+// }
+
+
+// function handleimg(ele ,event){
+//     // console.log("handleimg" , ele ,event)
+//     if(event === "over"){
+//         ele.src = "./images/car (2).jpg"
+//     }else{
+//         ele.src = "./images/car (1).jpg"
+//     }
+
+// }
+
+// function bulbstyling(btnelement){
+//     // console.log("btn" , btnelement);
+//     console.log(btnelement.innerhTML, "btn text");
+//     var bulbimg = document.getElementById("bulbimg")
+//     if(btnelement.innerHTML == "ON"){
+//         bulbimg.src = "./images/bulb (2).jpg";
+//         btnelement.innerHTML = "OFF";
+//     } else {
+//         bulbimg.src = "./images/bulb (1).jpg";
+//         btnelement.innerHTML = "ON";
+//     }
+// }
+
+
+
+
+
