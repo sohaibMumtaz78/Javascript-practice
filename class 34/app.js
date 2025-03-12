93d92f810a4a6c3af5fef689d0add296e2e90659
sohaibmumtaz78 <sohaibmumtaz782gmail.com>
2025-03-12 19:40:14 +0500

    DOM start

---
+++ class 34/app.js
@@ -0,0 +1,11 @@
+// console.log("Hello World")
+
+// DOM = DOcument object Model
+
+// console.log(document.childNodes[1].childNodes[2].childNodes[1])
+
+var edit = document.childNodes[1].childNodes[2].childNodes[1];
+// console.log(style)
+edit.style.color = "red"
+
+
