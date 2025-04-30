2c093506938e9f19cf6a1bd3ff89cc757f053dae
sohaibmumtaz78 <sohaibmumtaz782gmail.com>
2025-04-30 15:44:01 +0500

    reinitialize & function in obj

---
+++ Class 46/app.js
@@ -0,0 +1,59 @@
+// console.log("Class 46")
+
+//reinitialize 
+
+// var stdobj = {
+//     name : "sohaib",
+//     age : 20,
+//     gender : "Male",
+// }
+// console.log(stdobj)
+
+// stdobj.name = "Alishba" // reinitialize the name
+// stdobj.age = 25
+// stdobj.gender = "Female"
+
+// console.log(stdobj)
+
+// reinitialize in obj
+// var stdobj = {
+//     gener : "Male",
+//     age : 25,
+//     name : "Sohaib",
+//     name : "Sohaib Mumtaz",
+// }
+
+// // console.log("Name is" ,stdobj.name)
+
+// // for find all keys in obj use loop
+// for ( var key in stdobj){
+//     console.log("Keys value " , stdobj[key])
+// }
+
+
+
+// //using functio in obj
+
+// var stdobj = {
+//     name : "Sohaib Mumtaz",
+//     rollno : "WMA-325219",
+//     Subjects : ["Html" , "CSS" , "JAVASCRIPT"],
+//     marks : {
+//         Html : 80,
+//         CSS : 72,
+//         JAVASCRIPT : 92,
+//     },
+//     getName : function(){
+//         console.log("Hello world")
+//     }
+
+// }
+// // console.log(stdobj)
+// stdobj.getName()
+
+
+// console.log(stdobj.Subjects["0"] , stdobj.marks.Html) // get the marks of html using function
+// console.log(stdobj.Subjects[1] , stdobj.marks.CSS) // get the marks of css using function
+// console.log(stdobj.Subjects[2] , stdobj.marks.JAVASCRIPT) // get the marks of JAVASCRIPT using function
+
+
