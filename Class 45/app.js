f7e1f5d4da8ef23fcdb1d8ff11cec72202c1444d
sohaibmumtaz78 <sohaibmumtaz782gmail.com>
2025-04-30 15:42:35 +0500

    in and operator

---
+++ Class 45/app.js
@@ -0,0 +1,35 @@
+// console.log("Class 45")
+
+
+    var obj = {
+        name : "Sohaib",
+        age : 20,
+        gender : "male",
+    }
+
+// console.log(obj) // for full obj
+// console.log(obj.name) //for call name
+// console.log(obj.age)    //for call age
+// console.log(obj.gender)    //for call gender
+
+// IN and Delete in Obj
+
+//In operator in obj
+
+// "name" in obj
+// console.log("name" in obj)   // in operator check that this property exists in this or not
+// console.log("age" in obj)
+// console.log("gender" in obj)
+// console.log(obj)
+
+// Delete operator in Obj
+// console.log(delete obj.name) // Delete operator delete the property
+// console.log(delete obj.age)
+// console.log(delete obj.gender)
+
+// console.log(obj)
+
+
+
+
+
