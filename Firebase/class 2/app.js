0008a6015e7434ef1bda908e96735f5699bae7c6
sohaibmumtaz78 <sohaibmumtaz782gmail.com>
2025-07-11 02:44:41 +0500

    firebase assignment complete

--- Firebase/class 2/app.js
+++ Firebase/class 2/app.js
@@ -24,7 +24,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getFirestore(app);
-let numbersColletion = collection(db, 'numbers')
+// let numbersColletion = collection(db, 'numbers')
 let todoCollection = collection(db, "todos")
 
 
