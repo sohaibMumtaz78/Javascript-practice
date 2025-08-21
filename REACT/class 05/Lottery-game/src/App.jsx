9081f2534c9c6c58f54ec70bb17471825945337d
sohaibmumtaz78 <sohaibmumtaz782gmail.com>
2025-08-22 02:06:47 +0500

    class 05 comp

--- REACT/class 05/Lottery-game/src/App.jsx
+++ REACT/class 05/Lottery-game/src/App.jsx
@@ -1,12 +1,21 @@
 import { useState } from 'react'
 import Lottery from './Lottery'
+// import TicketNum from './TicketNum'
+import Ticket from './Ticket'
 import './App.css'
+import { sum } from './helper'
 
 function App() {
+  let winCondition = (ticket) => {
+    return ticket[0] === 0
+    // ticket.every((num)=> num=== ticket[0]) condition k first num k bad waly same
+    // return sum(ticket) === 15
+  }
 
   return (
     <div>
-      <Lottery/>
+      {/* <Ticket ticket={[0, 1, 2]}/> */}
+      <Lottery n={3} winCondition={winCondition} />
     </div>
   )
 }
--- REACT/class 05/Lottery-game/src/Lottery.jsx
+++ REACT/class 05/Lottery-game/src/Lottery.jsx
@@ -1,25 +1,25 @@
 import { useState } from "react";
 import { genTicket, sum } from "./helper";
-import './Lottery.css'
+// import './Lottery.css'
+import Ticket from "./Ticket";
+import Button from "./Button";
 
 
 
-function Lottery() {
-    let [ticket, setTicket] = useState(genTicket(3));
-    let winTicket = sum(ticket) === 15;
-    let buyTicket=()=>{
-        setTicket(genTicket(3))
+function Lottery({n = 3, winCondition}) {
+    let [ticket, setTicket] = useState(genTicket(n));
+    let winTicket = winCondition(ticket);
+    let buyTicket = () => {
+        setTicket(genTicket(n))
     }
     return (
         <div>
             <h1>Lottery Game!</h1>
-            <div className="ticket">
-                <span>{ticket[0]}</span>
-                <span>{ticket[1]}</span>
-                <span>{ticket[2]}</span>
-            </div>
-            <br />
-            <button onClick={buyTicket}>Buy new ticket</button>
+            <Ticket ticket={ticket} />
+            {/* <div className="ticket">
+            </div> */}
+            {/* <br /> */}
+            <Button action={buyTicket}/>
             <h3>{winTicket && "Congratulation, you won!"}</h3>
         </div>
 
